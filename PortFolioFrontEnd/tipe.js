8cc0cbcad0628e014307063acd81ad9e21c6f62c
Miqueas <miqueassantillan2704@gmail.com>
2022-11-24 15:36:24 -0300

    Avances

---
+++ PortFolioFrontEnd/tipe.js
@@ -0,0 +1,8 @@
+var mensaje;
+mensaje = "Usar TypeScript";
+console.log(mensaje);
+function saludo() {
+    console.log("hola mundo");
+}
+;
+saludo();
---
+++ PortFolioFrontEnd/tipe.ts
@@ -0,0 +1,7 @@
+let mensaje:string; mensaje="Usar TypeScript"
+console.log (mensaje)
+
+function saludo(){
+    console.log("hola mundo")};
+
+saludo();
